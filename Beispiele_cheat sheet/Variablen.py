4dd8950cd9a27c1e4d99a9d5279a6745dd8d90f6
BuildTools <unconfigured@null.spigotmc.org>
2022-05-26 00:07:40 +0200

    Variables

--- Beispiele_cheat sheet/Variablen.py
+++ Beispiele_cheat sheet/Variablen.py
@@ -1,21 +1,47 @@
 """"""""""
-Numbers
+Integers
 """
-x = 6 # int
-y = 3.14 # float
-x = x + 1 # add 1
-x += 1 # add 1
+x = 6  # this is a Integer
+
+x = x + 1  # add 1
+x += 1  # add 1
+
+""""""""""
+Float
+"""
+x = 3.14159265359  # this is a Float
+
+x = x + 1  # add 1
+x += 1  # add 1
 
 """""""""
 Strings
 """
-vorname = 'Max' #Simple String
-nachname = "Müller"#Simple String
-vollname = f'Mein Name ist {vorname} {nachname}' # f-String
+vorname = 'Max'  # Simple String
+nachname = "Müller"  # Simple String
+vollname = f'Mein Name ist {vorname} {nachname}'  # f-String
 
+"""""""""
+Conversions
+"""
 
+to_int = "4"
+to_float = "3.14"  # important you need a fullstop
+to_bool = "True"
+to_str = False
+to_list = "Max"
+to_tuple = ["Max", "Müller"]
 
+int(to_int)  # convert to Int
+float(to_float)  # convert to Float
+bool(to_bool)  # convert to Bool
+str(to_str)  # convert to String
+list(to_list)  # convert to List
+tuple(to_tuple)  # convert to Tuple
+print(type())  # Print Type Variables
 
-
-
+x = 6
+y = 3.14
+print(x)
+print(type(x))
 
