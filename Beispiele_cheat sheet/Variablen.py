2c444b780a9c04c8137f6e65a87cd07ee53a5a5b
Red <ericreichelt5@gmail.com>
2022-05-25 12:09:31 +0200

    Nubers and Strings

---
+++ Beispiele_cheat sheet/Variablen.py
@@ -0,0 +1,21 @@
+""""""""""
+Numbers
+"""
+x = 6 # int
+y = 3.14 # float
+x = x + 1 # add 1
+x += 1 # add 1
+
+"""""""""
+Strings
+"""
+vorname = 'Max' #Simple String
+nachname = "Müller"#Simple String
+vollname = f'Mein Name ist {vorname} {nachname}' # f-String
+
+
+
+
+
+
+
