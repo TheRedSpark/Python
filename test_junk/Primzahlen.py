a58c17ee063669eda7a8b91784056847ab7bf87a
BuildTools <unconfigured@null.spigotmc.org>
2022-06-01 23:19:35 +0200

    Initial Commit

---
+++ test_junk/Primzahlen.py
@@ -0,0 +1,29 @@
+primazhlen = []
+
+
+def isPrime(n):
+    if (n <= 1):
+        return False
+    if (n <= 3):
+        return True
+
+    if (n % 2 == 0 or n % 3 == 0):
+        return False
+
+    i = 5
+    while (i * i <= n):
+        if (n % i == 0 or n % (i + 2) == 0):
+            return False
+        i = i + 6
+
+    return True
+
+
+for i in range(1, 1000000000):
+    if isPrime(i) == True:
+        primazhlen.append(i)
+        print(i)
+    else:
+        continue
+
+print(primazhlen)
