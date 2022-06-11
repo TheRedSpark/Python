05f593b35b0d12b5d9197d079f621cc3e4447121
Red <ericreichelt5@gmail.com>
2022-06-11 11:45:02 +0200

    OS Ping

--- Überwachung/überwachung.py
+++ Überwachung/überwachung.py
@@ -12,8 +12,18 @@ mail_from = v.mail_from
 mail_to = v.mail_to
 mail_subject_server = v.mail_subject_server
 mail_subject_sql = v.mail_subject_sql
+mail_subject_offline = v.mail_subject_offline
 mail_body_sql = "Test überwachung"
 mail_body_server = "TTTT"
+mail_body_offline = "Offline"
+
+def ping(host):
+    response = os.system("ping -c 1 " + host)
+    if response == 0:
+        return True
+    else:
+        return False
+
 
 
 
@@ -44,19 +54,34 @@ def email_server():
     connection.quit()
     print('Email wind erfolg')
 
+    def email_offline():
+        mimemsg = MIMEMultipart()
+        mimemsg['From'] = mail_from
+        mimemsg['To'] = mail_to
+        mimemsg['Subject'] = mail_subject_offline
+        mimemsg.attach(MIMEText(mail_body_offline, 'plain'))
+        connection = smtplib.SMTP(host='mail.gmx.net', port=587)
+        connection.starttls()
+        connection.login(username, password)
+        connection.send_message(mimemsg)
+        connection.quit()
+        print('Email wind erfolg')
+
 #email_sql()
 while True:
-    if sql.zeitabstand(30,False) == True:
-        mail_body_sql = f'Der letzte Datensatz ist {sql.getZeitabstand()} her irgendein Problem muss bestehen'
-        email_sql()
-        print("Hi")
-        time.sleep(30)
-    elif sql.zeitabstand(30,False) == False:
-        pass
+    if ping(v.sqladdr):
+        if sql.zeitabstand(30,False) == True:
+            mail_body_sql = f'Der letzte Datensatz ist {sql.getZeitabstand()} her irgendein Problem muss bestehen'
+            email_sql()
+            print("Hi")
+            time.sleep(30)
+        elif sql.zeitabstand(30,False) == False:
+            pass
+        else:
+            print("Fehler")
+            sys.exit()
     else:
-        print("Fehler")
-        sys.exit()
-
+        email_offline()
 
     print(sql.getZeitabstand())
     print("Durchlauf")
