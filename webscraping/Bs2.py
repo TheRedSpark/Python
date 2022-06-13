6d7d9998c897d2e2c814dfcf381bd9b913c62df8
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-06-13 14:45:58 +0200

    Initial Commit

---
+++ webscraping/Bs2.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup # V4.10.0
+import requests # V2.27.1
+
+url = f'https://www.finanzen.net/devisen/bitcoin-euro-kurs'
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+table = soup.find('div', attrs={'class': 'col-xs-5 col-sm-4 text-sm-right text-nowrap'})
+rows = table.find_all('div')
+
+#print(soup)
+print(table)
+print(str(rows).replace("</span></div>]","").replace("<span>","").replace("[<div>",""))
