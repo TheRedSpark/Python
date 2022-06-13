e7d1184ab2bfb00ee8d8e5a1059ba309f810e382
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-06-13 18:35:35 +0200

    -

--- test_junk/time.py
+++ test_junk/time.py
@@ -4,11 +4,14 @@ trigger = time.gmtime()
 x = trigger.tm_mday
 #print(x)
 #day = 33
+#print(type(trigger.tm_min))
 day = int(trigger.tm_min)
 while True:
-    if trigger.tm_mday != day:
-        trigger.tm_mday = day
+    if trigger.tm_min != day:
+        day = trigger.tm_min
         print("Tag ist ungleich")
     else:
         print("Tag ist gleich")
-    time.sleep(10)
+    time.sleep(5)
+    print(trigger.tm_min)
+    print(day)
