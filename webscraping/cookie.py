de204cd977995614813198d3d1ad03154ed086ed
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-06-20 18:46:53 +0200

    automatische Iteration von Gegenständen

--- webscraping/cookie.py
+++ webscraping/cookie.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+i = 0
 from package import variables as v
 base_url = 'https://orteil.dashnet.org/cookieclicker/'
 browser = webdriver.Chrome(ChromeDriverManager().install())
@@ -10,18 +11,25 @@ browser.find_element_by_xpath("/html/body/div[1]/div/a[1]").click()#cookies
 browser.find_element_by_id("langSelect-DE").click()#sprache
 time.sleep(10)
 bigcookie = browser.find_element_by_id("bigCookie")
-cursor = browser.find_element_by_xpath("/html/body/div/div[2]/div[19]/div[3]/div[6]/div[2]")
-oma = browser.find_element_by_xpath("/html/body/div/div[2]/div[19]/div[3]/div[6]/div[3]")
-farm = browser.find_element_by_xpath("/html/body/div/div[2]/div[19]/div[3]/div[6]/div[4]")
 for a in range(1000):
     bigcookie.click()
     time.sleep(0.01)
 #oma.click()
-cookies = int(browser.title.replace(" Kekse - Cookie Clicker", ""))
+
+a = 2
+price = browser.find_element_by_id("productPrice1").text
+print(price)
 while True:
-    cursor.click()
-    oma.click()
-    farm.click()
-    time.sleep(3)
+    cookies = int(str(browser.title.replace(" Kekse - Cookie Clicker", "")).replace(",",""))
+    if i < 10 and int(str(browser.find_element_by_id(f'productPrice{a-2}').text).replace(",","")) < cookies:
+        browser.find_element_by_xpath(f'/html/body/div/div[2]/div[19]/div[3]/div[6]/div[{a}]').click()
+        i += 1
+    elif i >= 10:
+        a += 1
+        i = 0
+        print("")
+    else:
+        pass
+    time.sleep(0.5)
 print(cookies)
 #browser.close()
