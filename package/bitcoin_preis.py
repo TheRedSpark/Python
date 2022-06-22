a51c479d983c120e08e72932dde740c581a927a1
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-06-22 16:31:23 +0200

    Recode

--- package/bitcoin_preis.py
+++ package/bitcoin_preis.py
@@ -7,11 +7,7 @@ url = f'https://www.finanzen.net/devisen/bitcoin-euro-kurs'
 def btc():
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    table = soup.find('div', attrs={'class': 'col-xs-5 col-sm-4 text-sm-right text-nowrap'})
-    rows = table.find_all('div')
-
-    price = float(
-        str(rows).replace("</span></div>]", "").replace("<span>", "").replace("[<div>", "").replace("EUR", "").replace(
-            ".", "").replace(",", "."))
+    table = soup.find('div', attrs={'class': 'col-xs-5 col-sm-4 text-sm-right text-nowrap'}).text.replace("Euro","").replace(".","").replace(",",".")
+    price = float
 
     return price
