25ec28ec4b05fb70a247ac72d5dc4200aa9a0630
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-06-24 09:27:03 +0200

    -

--- telegram_botv2/testbot.py
+++ telegram_botv2/testbot.py
@@ -38,7 +38,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [
         [
             InlineKeyboardButton("Option 1", callback_data="1")],
-            [InlineKeyboardButton("Option 2", callback_data="2")],
+            [InlineKeyboardButton("Option 2", callback_data="2")]
         ,
         [InlineKeyboardButton("Option 3", callback_data="3")],
     ]
