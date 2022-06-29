9b599992f063aebd5bf86fddf8717fae56adf3ac
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-06-29 14:48:04 +0200

    User Create

--- selmabot/main.py
+++ selmabot/main.py
@@ -8,7 +8,7 @@ import mysql.connector
 import time
 
 ort = "home"
-database = "Telegram"
+database = "Selma"
 live = False
 
 try:
@@ -31,28 +31,59 @@ logging.basicConfig(
 
 
 def userlogging(user_id, username, message_chat_id, message_txt, message_id, first_name, last_name, land_code):
-    if live:
-        mydb = mysql.connector.connect(
-            host=v.host(ort),
-            user=v.user(ort),
-            passwd=v.passwd(ort),
-            database=v.database(database),
-            auth_plugin='mysql_native_password')
+    mydb = mysql.connector.connect(
+        host=v.host(ort),
+        user=v.user(ort),
+        passwd=v.passwd(ort),
+        database=v.database(database),
+        auth_plugin='mysql_native_password')
 
-        my_cursor = mydb.cursor()
-        time_sql = time.strftime("%Y-%m-%d %H:%M:%S")
-        sql_maske = "INSERT INTO `Telegram`.`Messages` (`Time`,`User_Id`,`Username`,`Chat_Id`,`Message_Text`," \
+    my_cursor = mydb.cursor()
+    time_sql = time.strftime("%Y-%m-%d %H:%M:%S")
+    if live:
+
+        sql_maske = "INSERT INTO `Selma`.`Messages` (`Time`,`User_Id`,`Username`,`Chat_Id`,`Message_Text`," \
                     "`Message_Id`,`First_Name`,`Last_Name`,`Land_Code`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s); "
-        data_n = (time_sql, user_id, username, message_chat_id, message_txt, message_id, first_name, last_name, land_code)
+        data_n = (
+            time_sql, user_id, username, message_chat_id, message_txt, message_id, first_name, last_name, land_code)
         my_cursor.execute(sql_maske, data_n)
         mydb.commit()
+
+    else:
+
+        sql_maske = "INSERT INTO `Telegram`.`Messagesbeta` (`Time`,`User_Id`,`Username`,`Chat_Id`,`Message_Text`," \
+                    "`Message_Id`,`First_Name`,`Last_Name`,`Land_Code`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s); "
+        data_n = (
+            time_sql, user_id, username, message_chat_id, message_txt, message_id, first_name, last_name, land_code)
+        my_cursor.execute(sql_maske, data_n)
+        mydb.commit()
+
+
+def usercreate(user_id, username):
+    mydb = mysql.connector.connect(
+        host=v.host(ort),
+        user=v.user(ort),
+        passwd=v.passwd(ort),
+        database=v.database(database),
+        auth_plugin='mysql_native_password')
+
+    my_cursor = mydb.cursor()
+    time_sql = time.strftime("%Y-%m-%d %H:%M:%S")
+
+    sql_maske = "INSERT INTO `Selma`.`Users` (`User_Id`,`Username`) VALUES (%s, %s); "
+    data_n = (user_id, username)
+    my_cursor.execute(sql_maske, data_n)
+    mydb.commit()
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     userlogging(update.effective_user.id, update.effective_user.username, update.effective_message.chat_id,
                 update.effective_message.text_markdown, update.effective_message.id, update.effective_user.first_name,
                 update.effective_user.last_name, update.effective_user.language_code)
+    usercreate(update.effective_user.id,update.effective_user.username)
     await update.message.reply_text('Benutze /help um diese Nachricht anzuzeigen'
                                     'Benutze /msg <Nachricht> um die Nachricht an den Developer zu schicken\n')
+
 
 async def msg(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     userlogging(update.effective_user.id, update.effective_user.username, update.effective_message.chat_id,
@@ -65,14 +96,15 @@ async def msg(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
         f'Deine Nachricht {update.message.text.replace("/msg ", "")} wurde an den Developer geschickt!')
 
+
 def main() -> None:
-    application = Application.builder().token(v.telegram_selma_api).build()
+    application = Application.builder().token(v.telegram_selma_api(live)).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler(["start", "help"], start))
     application.add_handler(CommandHandler("msg", msg))
     # massage handler
-    #application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
+    # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling()
@@ -80,4 +112,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
