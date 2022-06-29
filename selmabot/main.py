09b60d19f62eac14019a10595caf6d42aecafa8f
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-06-29 15:22:25 +0200

    get Funktions for Userdata

--- selmabot/main.py
+++ selmabot/main.py
@@ -22,8 +22,8 @@ if __version_info__ < (20, 0, 0, "alpha", 1):
         f"{TG_VER} version von diesem Beispiel, "
         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
     )
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler, MessageHandler, filters
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -76,11 +76,96 @@ def usercreate(user_id, username):
     mydb.commit()
 
 
+def get_username(user_id):
+    mydb = mysql.connector.connect(
+        host=v.host(ort),
+        user=v.user(ort),
+        passwd=v.passwd(ort),
+        database=v.database(database),
+        auth_plugin='mysql_native_password')
+
+    my_cursor = mydb.cursor()
+    my_cursor.execute(f"SELECT Username_Selma FROM `Selma`.`Users` WHERE User_Id = ({user_id}) ")
+    result = my_cursor.fetchone()
+    return result[0]
+
+
+def get_userpass(user_id):
+    mydb = mysql.connector.connect(
+        host=v.host(ort),
+        user=v.user(ort),
+        passwd=v.passwd(ort),
+        database=v.database(database),
+        auth_plugin='mysql_native_password')
+
+    my_cursor = mydb.cursor()
+    my_cursor.execute(f"SELECT Password_Selma FROM `Selma`.`Users` WHERE User_Id = ({user_id}) ")
+    result = my_cursor.fetchone()
+    return result[0]
+
+
+def get_user_email(user_id):
+    mydb = mysql.connector.connect(
+        host=v.host(ort),
+        user=v.user(ort),
+        passwd=v.passwd(ort),
+        database=v.database(database),
+        auth_plugin='mysql_native_password')
+
+    my_cursor = mydb.cursor()
+    my_cursor.execute(f"SELECT Email FROM `Selma`.`Users` WHERE User_Id = ({user_id}) ")
+    result = my_cursor.fetchone()
+    return result[0]
+
+
+async def setmenu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Sends a message with three inline buttons attached."""
+    keyboard = [
+        [InlineKeyboardButton("Benutzernamen Selma", callback_data="m1")],
+        [InlineKeyboardButton("Passwort Selma", callback_data="m2")],
+        [InlineKeyboardButton("Email setzen", callback_data="m3")],
+        [InlineKeyboardButton("Daten löschen", callback_data="m1")],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text("Bitte wählen:", reply_markup=reply_markup)
+
+
+async def menu_actions(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    await query.answer()
+
+    if query.data == 'm1':
+        # first submenu
+        get_username(update.effective_user.id)
+
+    elif query.data == 'm1_1':
+        # second submenu
+        # first submenu
+        menu_2 = [[InlineKeyboardButton('Submenu 2-1', callback_data='m2_1')],
+                  [InlineKeyboardButton('Submenu 2-2', callback_data='m2_2')]]
+        reply_markup = InlineKeyboardMarkup(menu_2)
+        await query.edit_message_text(text='Choose the option:', reply_markup=reply_markup)
+
+    elif query.data == 'm2_1':
+        # second submenu
+        # first submenu
+        await query.answer("Pl input number")
+        zahl = query.data
+        print(zahl)
+
+    else:
+        await query.edit_message_text(text=f"Selected option: {query.data}")
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     userlogging(update.effective_user.id, update.effective_user.username, update.effective_message.chat_id,
                 update.effective_message.text_markdown, update.effective_message.id, update.effective_user.first_name,
                 update.effective_user.last_name, update.effective_user.language_code)
-    usercreate(update.effective_user.id,update.effective_user.username)
+    usercreate(update.effective_user.id, update.effective_user.username)
     await update.message.reply_text('Benutze /help um diese Nachricht anzuzeigen'
                                     'Benutze /msg <Nachricht> um die Nachricht an den Developer zu schicken\n')
 
@@ -103,6 +188,8 @@ def main() -> None:
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler(["start", "help"], start))
     application.add_handler(CommandHandler("msg", msg))
+    application.add_handler(CommandHandler("menu", setmenu))
+    application.add_handler(CallbackQueryHandler(menu_actions))
     # massage handler
     # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
 
