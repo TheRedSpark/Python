6d2201271122e07ff5fc4d7451a65fba86d81b34
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-06-29 12:01:22 +0200

    Initial Commit

---
+++ selmabot/main.py
@@ -0,0 +1,83 @@
+# V1.0
+
+import logging
+from package import variables as v
+from telegram import __version__ as TG_VER  # v20
+import mysql.connector
+
+import time
+
+ort = "home"
+database = "Telegram"
+live = False
+
+try:
+    from telegram import __version_info__
+except ImportError:
+    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+
+if __version_info__ < (20, 0, 0, "alpha", 1):
+    raise RuntimeError(
+        f"Dieses Beispiel ist nicht kompatibel mit deiner PTB version {TG_VER}."
+        f"{TG_VER} version von diesem Beispiel, "
+        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
+    )
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+
+def userlogging(user_id, username, message_chat_id, message_txt, message_id, first_name, last_name, land_code):
+    if live:
+        mydb = mysql.connector.connect(
+            host=v.host(ort),
+            user=v.user(ort),
+            passwd=v.passwd(ort),
+            database=v.database(database),
+            auth_plugin='mysql_native_password')
+
+        my_cursor = mydb.cursor()
+        time_sql = time.strftime("%Y-%m-%d %H:%M:%S")
+        sql_maske = "INSERT INTO `Telegram`.`Messages` (`Time`,`User_Id`,`Username`,`Chat_Id`,`Message_Text`," \
+                    "`Message_Id`,`First_Name`,`Last_Name`,`Land_Code`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s); "
+        data_n = (time_sql, user_id, username, message_chat_id, message_txt, message_id, first_name, last_name, land_code)
+        my_cursor.execute(sql_maske, data_n)
+        mydb.commit()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    userlogging(update.effective_user.id, update.effective_user.username, update.effective_message.chat_id,
+                update.effective_message.text_markdown, update.effective_message.id, update.effective_user.first_name,
+                update.effective_user.last_name, update.effective_user.language_code)
+    await update.message.reply_text('Benutze /help um diese Nachricht anzuzeigen'
+                                    'Benutze /msg <Nachricht> um die Nachricht an den Developer zu schicken\n')
+
+async def msg(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    userlogging(update.effective_user.id, update.effective_user.username, update.effective_message.chat_id,
+                update.effective_message.text_markdown, update.effective_message.id, update.effective_user.first_name,
+                update.effective_user.last_name, update.effective_user.language_code)
+    chat_id = v.telegram_user_id
+    await context.bot.send_message(chat_id,
+                                   text=f'User:({update.effective_user.username}) hat dir ({update.message.text.replace("/msg ", "")}) '
+                                        f'geschickt!')
+    await update.message.reply_text(
+        f'Deine Nachricht {update.message.text.replace("/msg ", "")} wurde an den Developer geschickt!')
+
+def main() -> None:
+    application = Application.builder().token(v.telegram_selma_api).build()
+
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler(["start", "help"], start))
+    application.add_handler(CommandHandler("msg", msg))
+    # massage handler
+    #application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
+
