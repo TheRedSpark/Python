cba7560e25e5a5df9cbd0e4373c548462ef9ce18
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-07-31 22:33:59 +0200

    Gui

--- test_junk/gui.py
+++ test_junk/gui.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 
-
 root = tk.Tk()
 root.title("Das ist ein Testfenster")
 root.geometry("600x800")
-label1 = tk.Label(root, text="Hallo Welt")
+root.minsize(width=250, height=250)
+root.maxsize(width=250, height=250)
+root.resizable(width=False,height=False)
+label1 = tk.Label(root, text="Hallo Welt",bg="green")
 label1.pack()
 root.mainloop()
