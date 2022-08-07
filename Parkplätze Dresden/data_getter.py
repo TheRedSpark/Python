beb69014d29009dd2ec41ae8af44e39620ecdf3f
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-08-07 19:38:38 +0200

    Call Working

--- Parkplätze Dresden/data_getter.py
+++ Parkplätze Dresden/data_getter.py
@@ -4,13 +4,25 @@ import time
 import mysql.connector  # V8.0.28
 from package import zugang as anbin
 
-
 url = "https://www.dresden.de/apps_ext/ParkplatzApp/index"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-#print(soup)
-table = soup.find('div', attrs={'column size8'})
-option = table.find_all("Innere Altstadt")
-#print(option)
-print(table)
-#rows = table.find_all('tr')
+
+table = soup.find_all('div', attrs={'class': 'element element_table responsiveTable'})
+
+#print(len(table))
+i = 0
+a = 0
+for orte in table:
+    rows = table[i].find_all('tr')
+    i = i + 1
+    for platze in rows:
+        simple_rows = rows[a].text.split("\n")
+        innere_altstadt = [feld for feld in simple_rows if feld != '']
+        if a == 0:
+            print(innere_altstadt[0])
+        else:
+            print(innere_altstadt)
+            # print(a)
+        a = a + 1
+    a = 0
