ba583601f92a1f3a9b4f16873ca080e4b11450b6
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-08-14 15:11:57 +0200

    pyinstrument

--- test_junk/debug_test.py
+++ test_junk/debug_test.py
@@ -1,4 +1,19 @@
+from pyinstrument import Profiler
+
+
 x = 5
 
 
 print(f'Das ist x: {x}')
+print(f'Das ist x: {x=}')
+
+
+
+profiler = Profiler()
+profiler.start()
+
+# code you want to profile
+
+profiler.stop()
+
+profiler.print()
