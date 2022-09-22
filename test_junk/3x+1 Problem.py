d5e5ea5f3b4189a4a4c6563ef7e73d48980aa271
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-09-22 21:22:13 +0200

    V1.1

--- test_junk/3x+1 Problem.py
+++ test_junk/3x+1 Problem.py
@@ -1,10 +1,13 @@
-zahl = input("Welche Zahl")
-
+zahl = int(input("Welche Zahl"))
+steps = 0
 while True:
     if zahl == 1:
         break
     if zahl % 2 == 0:
         zahl = zahl / 2
+        steps = +1
     else:
         zahl = zahl * 3 + 1
+        steps = +1
     print(zahl)
+print(f"Die Anzahl der Schritte {steps}")
