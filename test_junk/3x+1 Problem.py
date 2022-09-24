31e68d93d8e22b544602d33a200bee90f3eb4e63
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-09-24 19:21:27 +0200

    V1.4

--- test_junk/3x+1 Problem.py
+++ test_junk/3x+1 Problem.py
@@ -1,16 +1,19 @@
-anzahl_zahlen = int(input("Bis zu Welcher Zahl?"))
-x = range(1, anzahl_zahlen)
-print(x)
-for zahl in x:
-    steps = []
-    steps.append(zahl)
-    while True:
-        if zahl == 1:
-            break
-        if zahl % 2 == 0:
-            zahl = zahl / 2
-            steps.append(zahl)
-        else:
-            zahl = zahl * 3 + 1
-            steps.append(zahl)
-    print(f"{len(steps)} Schritte für {steps[0]} mit der Liste{steps}")
+
+
+def rechner(zahlen) -> None:
+    for zahl in zahlen:
+        steps = []
+        steps.append(zahl)
+        while True:
+            if zahl == 1:
+                break
+            if zahl % 2 == 0:
+                zahl = zahl / 2
+                steps.append(zahl)
+            else:
+                zahl = zahl * 3 + 1
+                steps.append(zahl)
+        print(f"{len(steps)} Schritte für {steps[0]}")
+
+
+rechner(range(1, int(input("Bis zu welcher Zahl"))))
