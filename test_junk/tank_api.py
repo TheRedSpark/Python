5f49e31c1181158093d791c6108c031147195b38
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-03 15:46:52 +0200

    Uploader Funktion

--- test_junk/tank_api.py
+++ test_junk/tank_api.py
@@ -12,15 +12,12 @@ data = json.loads(res.content.decode())
 pprint(data)
 print(data["stations"][0]["brand"])
 print(data["stations"][1]["brand"])
-print(data["stations"][2]["brand"])
+print(type(data["stations"][2]["isOpen"]))
 ort = "home"
 database = "Tankdaten"
 
 
-
-
-
-def data_uploader(brand, diesel, e10, e5, id, isopen, lat, lng) -> None:
+def data_uploader(brand, diesel, e10, e5, id, isopen, lat, lng,name,postcode) -> None:
     zeit = time.strftime("%Y-%m-%d %H:%M:%S")
     with mysql.connector.connect(
             host=v.host(ort),
@@ -31,6 +28,13 @@ def data_uploader(brand, diesel, e10, e5, id, isopen, lat, lng) -> None:
         my_cursor = mydb.cursor()
         sql_stuff = (f"INSERT INTO `Tankdaten`.`Data` (`Zeit`,`brand`,`diesel`,`e10`,`e5`,`id`,`isOpen`,`lat`,`lng`,"
                      f"`name`,`postCode`) VALUES (%s, %s ,%s, %s, %s, %s, %s, %s, %s, %s, %s);")
-        record = (zeit, brand, diesel)
+        record = (zeit, brand, diesel,e10,e5,id,isopen,lat,lng,name,postcode)
         my_cursor.execute(sql_stuff, record)
         mydb.commit()
+
+
+for tanke in data["stations"]:
+    data_uploader(tanke["brand"], tanke["diesel"], tanke["e10"], tanke["e5"], tanke["id"], tanke["isOpen"],
+                  tanke["lat"], tanke["lng"], tanke["name"],tanke["postCode"])
+
+
