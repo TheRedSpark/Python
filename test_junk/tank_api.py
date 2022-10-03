b7271063d323fd6fba238df5ee087c694c56dd8c
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-03 22:48:28 +0200

    Uploader Funktion

--- test_junk/tank_api.py
+++ test_junk/tank_api.py
@@ -5,7 +5,10 @@ from pprint import pprint
 import mysql.connector
 import time
 import marshmallow
+from pyinstrument import Profiler
 
+profiler = Profiler()
+profiler.start()
 res = requests.get(
     'https://creativecommons.tankerkoenig.de/json/list.php?lat=52.521&lng=13.438&rad=25&sort=dist&type=all&apikey=' + v.tanke_api)
 data = json.loads(res.content.decode())
@@ -17,7 +20,17 @@ ort = "home"
 database = "Tankdaten"
 
 
-def data_uploader(brand, diesel, e10, e5, id, isopen, lat, lng,name,postcode) -> None:
+def commiter() -> None:
+    with mysql.connector.connect(
+            host=v.host(ort),
+            user=v.user(ort),
+            passwd=v.passwd(ort),
+            database=v.database(database),
+            auth_plugin='mysql_native_password') as mydb:
+        mydb.commit()
+
+
+def data_uploader(brand, diesel, e10, e5, id, isopen, lat, lng, name, postcode) -> None:
     zeit = time.strftime("%Y-%m-%d %H:%M:%S")
     with mysql.connector.connect(
             host=v.host(ort),
@@ -28,13 +41,16 @@ def data_uploader(brand, diesel, e10, e5, id, isopen, lat, lng,name,postcode) ->
         my_cursor = mydb.cursor()
         sql_stuff = (f"INSERT INTO `Tankdaten`.`Data` (`Zeit`,`brand`,`diesel`,`e10`,`e5`,`id`,`isOpen`,`lat`,`lng`,"
                      f"`name`,`postCode`) VALUES (%s, %s ,%s, %s, %s, %s, %s, %s, %s, %s, %s);")
-        record = (zeit, brand, diesel,e10,e5,id,isopen,lat,lng,name,postcode)
+        record = (zeit, brand, diesel, e10, e5, id, isopen, lat, lng, name, postcode)
         my_cursor.execute(sql_stuff, record)
-        mydb.commit()
 
 
 for tanke in data["stations"]:
     data_uploader(tanke["brand"], tanke["diesel"], tanke["e10"], tanke["e5"], tanke["id"], tanke["isOpen"],
-                  tanke["lat"], tanke["lng"], tanke["name"],tanke["postCode"])
+                  tanke["lat"], tanke["lng"], tanke["name"], tanke["postCode"])
 
+    commiter()
 
+profiler.stop()
+
+profiler.print(color=True, show_all=False)
