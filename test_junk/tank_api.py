26e348edcda599659f9469a853b4347b19b1e955
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-03 23:04:30 +0200

    Cleanup

--- test_junk/tank_api.py
+++ test_junk/tank_api.py
@@ -1,10 +1,8 @@
 import requests
 from package import variables as v
 import json
-from pprint import pprint
 import mysql.connector
 import time
-import marshmallow
 from pyinstrument import Profiler
 
 profiler = Profiler()
@@ -12,17 +10,12 @@ profiler.start()
 res = requests.get(
     'https://creativecommons.tankerkoenig.de/json/list.php?lat=52.521&lng=13.438&rad=25&sort=dist&type=all&apikey=' + v.tanke_api)
 data = json.loads(res.content.decode())
-pprint(data)
-print(data["stations"][0]["brand"])
-print(data["stations"][1]["brand"])
-print(type(data["stations"][2]["isOpen"]))
 ort = "home"
 database = "Tankdaten"
 list = []
 
 
 def data_uploader(datalist):
-    zeit = time.strftime("%Y-%m-%d %H:%M:%S")
     with mysql.connector.connect(
             host=v.host(ort),
             user=v.user(ort),
@@ -32,16 +25,14 @@ def data_uploader(datalist):
         my_cursor = mydb.cursor()
         sql_stuff = (f"INSERT INTO `Tankdaten`.`Data` (`Zeit`,`brand`,`diesel`,`e10`,`e5`,`id`,`isOpen`,`lat`,`lng`,"
                      f"`name`,`postCode`) VALUES (%s, %s ,%s, %s, %s, %s, %s, %s, %s, %s, %s);")
-        #record = (zeit, brand, diesel, e10, e5, id, isopen, lat, lng, name, postcode)
         my_cursor.executemany(sql_stuff, datalist)
         mydb.commit()
-
 
 
 for tanke in data["stations"]:
     zeit = time.strftime("%Y-%m-%d %H:%M:%S")
     list.append((zeit, tanke["brand"], tanke["diesel"], tanke["e10"], tanke["e5"], tanke["id"], tanke["isOpen"],
-                  tanke["lat"], tanke["lng"], tanke["name"], tanke["postCode"]))
+                 tanke["lat"], tanke["lng"], tanke["name"], tanke["postCode"]))
 
 data_uploader(list)
 
