c68bd3e9aac8b801343722045125126f36bfda8b
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-03 23:03:53 +0200

    Execute Many Zeit runter für 1k Inserts auf 2.1 Sec

--- test_junk/tank_api.py
+++ test_junk/tank_api.py
@@ -18,19 +18,10 @@ print(data["stations"][1]["brand"])
 print(type(data["stations"][2]["isOpen"]))
 ort = "home"
 database = "Tankdaten"
+list = []
 
 
-def commiter() -> None:
-    with mysql.connector.connect(
-            host=v.host(ort),
-            user=v.user(ort),
-            passwd=v.passwd(ort),
-            database=v.database(database),
-            auth_plugin='mysql_native_password') as mydb:
-        mydb.commit()
-
-
-def data_uploader(brand, diesel, e10, e5, id, isopen, lat, lng, name, postcode) -> None:
+def data_uploader(datalist):
     zeit = time.strftime("%Y-%m-%d %H:%M:%S")
     with mysql.connector.connect(
             host=v.host(ort),
@@ -41,15 +32,18 @@ def data_uploader(brand, diesel, e10, e5, id, isopen, lat, lng, name, postcode) 
         my_cursor = mydb.cursor()
         sql_stuff = (f"INSERT INTO `Tankdaten`.`Data` (`Zeit`,`brand`,`diesel`,`e10`,`e5`,`id`,`isOpen`,`lat`,`lng`,"
                      f"`name`,`postCode`) VALUES (%s, %s ,%s, %s, %s, %s, %s, %s, %s, %s, %s);")
-        record = (zeit, brand, diesel, e10, e5, id, isopen, lat, lng, name, postcode)
-        my_cursor.execute(sql_stuff, record)
+        #record = (zeit, brand, diesel, e10, e5, id, isopen, lat, lng, name, postcode)
+        my_cursor.executemany(sql_stuff, datalist)
+        mydb.commit()
+
 
 
 for tanke in data["stations"]:
-    data_uploader(tanke["brand"], tanke["diesel"], tanke["e10"], tanke["e5"], tanke["id"], tanke["isOpen"],
-                  tanke["lat"], tanke["lng"], tanke["name"], tanke["postCode"])
+    zeit = time.strftime("%Y-%m-%d %H:%M:%S")
+    list.append((zeit, tanke["brand"], tanke["diesel"], tanke["e10"], tanke["e5"], tanke["id"], tanke["isOpen"],
+                  tanke["lat"], tanke["lng"], tanke["name"], tanke["postCode"]))
 
-    commiter()
+data_uploader(list)
 
 profiler.stop()
 
