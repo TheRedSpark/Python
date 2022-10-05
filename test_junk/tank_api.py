a0b4c9852bda883aaae7a12e3b409d5b818733da
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-05 23:51:46 +0200

    Multi Radius Abfrage

--- test_junk/tank_api.py
+++ test_junk/tank_api.py
@@ -11,11 +11,9 @@ profiler = Profiler()
 profiler.start()
 lat = '52.506'
 lng = '13.284'
-# cords = [('52.506','13.284'), #berlin
-#          ('51.076','13.632') #dresden
-#          ('48.779','9.107')] #stuttgart
-
-
+cords = [('52.506','13.284',"berlin"), #berlin
+          ('51.076','13.632',"dresden"), #dresden
+          ('48.779','9.107',"stuttgart")] #stuttgart
 
 
 
@@ -35,17 +33,20 @@ def data_uploader(datalist):
 
 
 
-berlin_res = requests.get(f'https://creativecommons.tankerkoenig.de/json/list.php?lat={lat}&lng={lng}8&rad=25&sort=dist&type=all&apikey=' + v.tanke_api)
-data = json.loads(berlin_res.content.decode())
 
 
-for tanke in data["stations"]:
-    zeit = time.strftime("%Y-%m-%d %H:%M:%S")
-    list.append((zeit, tanke["brand"], tanke["diesel"], tanke["e10"], tanke["e5"], tanke["id"], tanke["isOpen"],
-                 tanke["lat"], tanke["lng"], tanke["name"], tanke["postCode"]))
+for rad in cords:
+    print(f"Jetzt {rad[2]}")
+    berlin_res = requests.get(
+        f'https://creativecommons.tankerkoenig.de/json/list.php?lat={rad[0]}&lng={rad[1]}8&rad=25&sort=dist&type=all&apikey=' + v.tanke_api)
+    data = json.loads(berlin_res.content.decode())
+    for tanke in data["stations"]:
+        zeit = time.strftime("%Y-%m-%d %H:%M:%S")
+        list.append((zeit, tanke["brand"], tanke["diesel"], tanke["e10"], tanke["e5"], tanke["id"], tanke["isOpen"],
+                     tanke["lat"], tanke["lng"], tanke["name"], tanke["postCode"]))
 
 data_uploader(list)
 
 profiler.stop()
 
-profiler.print(color=True, show_all=True)
+profiler.print(color=True, show_all=False)
