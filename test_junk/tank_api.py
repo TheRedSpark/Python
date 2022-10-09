4d1b85347c3b1119ada622164c5a76e2fcb791e2
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-10 00:03:10 +0200

    Rebuild für bestätigung der Standorte

--- test_junk/tank_api.py
+++ test_junk/tank_api.py
@@ -23,8 +23,8 @@ cords = [('52.506', '13.284', "Berlin", "Berlin"),
          ('48.719', '10.733', "Donauwörth", "Bayern"),
          ('48.779', '9.107', "Stuttgart", "BadenW"),
          ('48.270', ',8.787', "Balingen", "BadenW"),
-         ('50.135', '8.802', " Hanau", "Hessen"),
-         ('50.486', '9.011', " Schotten", "Hessen"),
+         ('50.135', '8.802', "Hanau", "Hessen"),
+         ('50.486', '9.011', "Schotten", "Hessen"),
          ('49.965', ',8.172', "Mainz", "Reinland"),
          ('49.701', ',7.349', "Idar-Oberstein", "Reinland"),
          ('49.247', '6.912', "Saarbrücken", "Saarland"),
@@ -58,16 +58,26 @@ def data_uploader(datalist):
 
 
 for rad in cords:
-    print(f"Jetzt {rad[2]}")
-    berlin_res = requests.get(
-        f'https://creativecommons.tankerkoenig.de/json/list.php?lat={rad[0]}&lng={rad[1]}8&rad=25&sort=dist&type=all&apikey=' + v.tanke_api)
-    data = json.loads(berlin_res.content.decode())
-    for tanke in data["stations"]:
-        zeit = time.strftime("%Y-%m-%d %H:%M:%S")
-        list.append((zeit, tanke["brand"], tanke["diesel"], tanke["e10"], tanke["e5"], tanke["id"], tanke["isOpen"],
-                     tanke["lat"], tanke["lng"], tanke["name"], tanke["postCode"], rad[2], rad[3]))
+    while True:
+        try:
+            print(f"Jetzt {rad[2]}")
+            berlin_res = requests.get(
+                f'https://creativecommons.tankerkoenig.de/json/list.php?lat={rad[0]}&lng={rad[1]}&rad=25&sort=dist&type=all&apikey=' + v.tanke_api)
+            data = json.loads(berlin_res.content.decode())
+            print(data["stations"])
+            for tanke in data["stations"]:
+                zeit = time.strftime("%Y-%m-%d %H:%M:%S")
+                list.append((zeit, tanke["brand"], tanke["diesel"], tanke["e10"], tanke["e5"], tanke["id"], tanke["isOpen"],
+                             tanke["lat"], tanke["lng"], tanke["name"], tanke["postCode"], rad[2], rad[3]))
+            data_uploader(list)
+            list.clear()
+            print(f"Erfolg für {rad[2]}")
+            break
+        except:
+            print(f"Fehler für {rad[2]}")
+            time.sleep(5)
+            continue
 
-data_uploader(list)
 
 profiler.stop()
 
