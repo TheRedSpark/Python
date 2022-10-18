7c3020934c732df72adc0098d9c00a8ddac4b082
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-18 19:59:17 +0200

    Data is plottet from SQL Server

--- Dash datenauswertung/app1/app.py
+++ Dash datenauswertung/app1/app.py
@@ -2,30 +2,54 @@ import dash
 from dash import dcc
 from dash import html
 import pandas as pd
+import mysql.connector
+from package import variables as v
 
-data = pd.read_csv("avocado.csv")
-data = data.query("type == 'conventional' and region == 'Albany'")
-data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
-data.sort_values("Date", inplace=True)
+ort = "home"
+database = "Tankdaten"
+
+
+def data_getter():
+    with mysql.connector.connect(
+            host=v.host(ort),
+            user=v.user(ort),
+            passwd=v.passwd(ort),
+            database=v.database(database),
+            auth_plugin='mysql_native_password') as mydb:
+        my_cursor = mydb.cursor()
+        my_cursor.execute(f'SELECT zeit,diesel,e10,e5 FROM `Tankdaten`.`Data` WHERE id ="e3ab6579-0b05-492e-8e74-c46e47923e71" LIMIT 10')
+        df = pd.DataFrame(my_cursor.fetchall(), columns=['zeit', 'diesel', 'e10', 'e5'])
+        #print(df)
+        #print(type(df))
+        return df
+
+data = data_getter()
+#print(data["zeit"])
+#data = pd.read_csv("avocado.csv")
+#print(type(data))
+#data = data.query("type == 'conventional' and region == 'Albany'")
+#data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
+#data.sort_values("Date", inplace=True)
+#print(data)
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div(
     children=[
         html.H1(
-            children="Avocado Analytics",
+            children="Tankstellen Anal",
         ),
         html.P(
             children="Analyze the behavior of avocado prices"
-            " and the number of avocados sold in the US"
-            " between 2015 and 2018",
+                     " and the number of avocados sold in the US"
+                     " between 2015 and 2018",
         ),
         dcc.Graph(
             figure={
                 "data": [
                     {
-                        "x": data["Date"],
-                        "y": data["AveragePrice"],
+                        "x": data["e10"],
+                        "y": data["diesel"],
                         "type": "lines",
                     },
                 ],
@@ -36,8 +60,8 @@ app.layout = html.Div(
             figure={
                 "data": [
                     {
-                        "x": data["Date"],
-                        "y": data["Total Volume"],
+                        "x": data["e10"],
+                        "y": data["e5"],
                         "type": "lines",
                     },
                 ],
@@ -49,3 +73,5 @@ app.layout = html.Div(
 
 if __name__ == "__main__":
     app.run_server(debug=True)
+    #print(type(data))
+    print("Start")
