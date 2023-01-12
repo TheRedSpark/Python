54a17bb2b05643d6b524899778912669568c7ccf
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2023-01-12 19:55:41 +0100

    Netzfrquenz Ausgabe

--- test_junk/netzfrequenzmessung.py
+++ test_junk/netzfrequenzmessung.py
@@ -1,4 +1,10 @@
 import requests
 
 x = requests.get('https://netzfrequenzmessung.de:9081/frequenz02a.xml?c=916856')
-print(x.text)
+y = x.text.splitlines()
+#print(y)
+netzfrequenz = y[1].replace("<f2>", "").replace("</f2>", "")
+zeit = y[3].replace("<z> ", "").replace("</z>", "")
+
+print(zeit)
+print(netzfrequenz)
