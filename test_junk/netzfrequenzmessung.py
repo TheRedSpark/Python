e67b3cfbeafb195d84790bee46bcffb0282a00f6
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2023-02-13 17:56:49 +0100

    Bla.

--- test_junk/netzfrequenzmessung.py
+++ test_junk/netzfrequenzmessung.py
@@ -1,10 +1,25 @@
 import requests
+import math as Math
+import random
+import  time
 
-x = requests.get('https://netzfrequenzmessung.de:9081/frequenz02a.xml?c=916856')
-y = x.text.splitlines()
-#print(y)
-netzfrequenz = y[1].replace("<f2>", "").replace("</f2>", "")
-zeit = y[3].replace("<z> ", "").replace("</z>", "")
 
-print(zeit)
-print(netzfrequenz)
+def generate_url():
+    client = round(random.randint(0, 10000) * 10) * 31
+    print(client)
+    url = f'https://netzfrequenzmessung.de:9081/frequenz02a.xml?c={client}'
+    return url
+
+
+##Math.round(Math.random()*100000)*31
+while True:
+    x = requests.get(generate_url())
+    y = x.text.splitlines()
+    # print(y)
+    netzfrequenz = y[1].replace("<f2>", "").replace("</f2>", "")
+    zeit = y[3].replace("<z> ", "").replace("</z>", "")
+
+    print(zeit)
+    print(netzfrequenz)
+    time.sleep(1)
+
