97a1a965485651076977aaec3f320d00c198f3e9
erre893d <eric.reichelt1@mailbox.tu-dresden.de>
2023-03-08 22:30:57 +0100

    Fix: Verbindung in beide richtungen klappt nun

--- MQTT/test1.py
+++ MQTT/test1.py
@@ -3,6 +3,8 @@ import paho.mqtt.client as mqtt
 import ssl
 
 from package import variables as v
+
+
 # this is a helper method that catches errors and prints them
 # it is necessary because on_message is called by paho-mqtt in a different thread and exceptions
 # are not handled in that thread
@@ -16,6 +18,7 @@ def on_message_excepthandler(client, data, message):
         traceback.print_exc()
         raise
 
+
 # Callback function for receiving messages
 def on_message(client, data, message):
     print('Got message with topic "{}":'.format(message.topic))
@@ -25,10 +28,10 @@ def on_message(client, data, message):
 
 
 # Basic configuration of MQTT
-client = mqtt.Client(client_id=f"{v.mqtt_user}", clean_session=False,protocol=mqtt.MQTTv31)
+client = mqtt.Client(client_id=f"{v.mqtt_user}", clean_session=False, protocol=mqtt.MQTTv31)
 
 client.on_message = on_message_excepthandler  # Assign pre-defined callback function to MQTT client
-client.tls_set(tls_version=ssl.PROTOCOL_TLS)
+# client.tls_set(tls_version=ssl.PROTOCOL_TLS)
 client.username_pw_set(f'{v.mqtt_user}',
                        password=f'{v.mqtt_pass}')
 client.connect(f'{v.mqtt_server}', port=1883)
@@ -41,17 +44,16 @@ while True:
 
     if user_input == 'disconnect':
         break
+    elif user_input == 'message':
+        data_message = '{ "name":"John", ' \
+                       '"age":30, ' \
+                       '"city":"New York"}'
+        print(type(data_message))
 
-data_message = {
-    "from": "client",
-    "type": "ready"
-}
-
-client.publish("comtest/009", data_message)
+        client.publish("comtest/008", str(data_message))
 
 # you could add some code to send a message here
 
 client.loop_stop()
 client.disconnect()
 print("Connection closed, program ended!")
-
